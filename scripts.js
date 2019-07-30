3093ba6ca2bd8ab91121bd3d8b44dbba38f64040
Lars <larsmackenzie@gmail.com>
2019-07-30 13:29:23 -0400

    refactored to include checkAll() again, but separated functions out to make it run more efficiently and only when user pushes a recalculate button

--- scripts.js
+++ scripts.js
@@ -13,69 +13,77 @@ var columnsArray = [{id: 2, letter: 'b'}, {id: 3, letter: 'c'}, {id: 4, letter: 
 var currentColumn = null; 
 var categories = ['READ', 'WATCH', 'DISCUSS', 'COLLABORATE', 'PRACTICE', 'REVIEW', 'WORK', 'SUBMIT'];
 
+function filterKeys(object){
+  if (object.id === 3) {
+  var letterToUse = object.letter;
+   return letterToUse;
+  }}
+
 function getColumn(category){
-    var column = SpreadsheetApp.getActiveSheet().getActiveCell();
-    column = column.getColumn()
-    for (var i = 0; i < columnsArray.length; i++){
-      if(columnsArray[i].id === column){
-        currentColumn = columnsArray[i].letter;
-      }
-    }// end for loop
-    // if we have a currentColumn, use that value to get the whole range of text so we can analyze it
-    if(currentColumn !== null){
-      var input = currentColumn + '5:' + currentColumn + '24';
-     var text = SpreadsheetApp.getActiveSheet().getRange(input).getValues();
+  Logger.log('values: current column: ' + currentColumn + 'read:' + read + 'watch: ' + watch + 'discuss: ' + discuss + 'practice: ' + practice + 'collab: ' + collab + 'review: ' + review + 'work: ' + work + 'submit: ' + submit); 
+  var column = SpreadsheetApp.getActiveSheet().getActiveCell();
+  column = column.getColumn()
+  for (var i = 0; i < columnsArray.length; i++){
+    if(columnsArray[i].id === column){
+      currentColumn = columnsArray[i].letter;
     }
-  
-     var allText = [];
-      for (var i = 0; i < text.length; i++) {
-          allText.push(text[i]);
-      }
-      // next we need to search through the contents of each object to see if it contains a keyword that matches our category argument
-      var count = 0;
-      for (var j = 0; j < allText.length; j++) {
-          //iterating through the keys because the range is an object
-          for (value in allText[j]) {
-              // if the word isn't present, it will return a -1 (or -1.0 in Google Scripts)
-              if (allText[j][value].search(category) !== -1.0) {
-                  // if we don't get a -1.0, increase our counter so we know how many of these items we have 
-                  count++;
-              }
-          }
-      }
-   // depending on what the keyword is, we should put our count in a certain row. we take the first character of our input argument (the letter of the column) to figure out which column we should add our count to
-      if (category === 'READ') {
-          read = count; 
-          learningTotal = read + watch; 
-          SpreadsheetApp.getActiveSheet().getRange(currentColumn + '26').setValue(learningTotal);
-      }
-      if (category === 'WATCH') {
-          //watch is a 15 minute unit, so let's divide the 1 hour standard unit by 4 to get our accurate count for the watch category
-          watch = count/4; 
-          learningTotal = read + watch; 
-          // reading and watching are going to end up in the same category, but they use different keywords and different units of time. 
-          // add them together using the global scope variables so that we can get an accurate number for this combined category.
-          SpreadsheetApp.getActiveSheet().getRange(currentColumn + '26').setValue(learningTotal);
-        Logger.log(watch + 'watch')
-        Logger.log(learningTotal + 'learning total')
-      }
-      if (category === 'DISCUSS') {
-          discuss = count; 
-          discussCollabTotal = discuss + collab; 
-          SpreadsheetApp.getActiveSheet().getRange(currentColumn + '27').setValue(discussCollabTotal);
-      }
-      if (category === 'COLLABORATE') {
-          collab = count; 
-          // discussing and collaborating are going to end up in the same category, but they use different keywords. 
-          // add them together using the global scope variables so that we can get an accurate number for this combined category.
-          discussCollabTotal = discuss + collab; 
-          SpreadsheetApp.getActiveSheet().getRange(currentColumn + '27').setValue(discussCollabTotal);
-      }
-      if (category === 'PRACTICE') {
-          SpreadsheetApp.getActiveSheet().getRange(currentColumn + '28').setValue(count);
-          practice = count; 
-      }
-      if (category === 'REVIEW') {
+  }// end for loop
+  getValues(category);
+}
+function getValues(category){
+  // if we have a currentColumn, use that value to get the whole range of text so we can analyze it
+  if(currentColumn !== null){
+    var input = currentColumn + '5:' + currentColumn + '24';
+   var text = SpreadsheetApp.getActiveSheet().getRange(input).getValues();
+  }
+
+   var allText = [];
+    for (var i = 0; i < text.length; i++) {
+        allText.push(text[i]);
+    }
+    // next we need to search through the contents of each object to see if it contains a keyword that matches our category argument
+    var count = 0;
+    for (var j = 0; j < allText.length; j++) {
+        //iterating through the keys because the range is an object
+        for (value in allText[j]) {
+            // if the word isn't present, it will return a -1 (or -1.0 in Google Scripts)
+            if (allText[j][value].search(category) !== -1.0) {
+                // if we don't get a -1.0, increase our counter so we know how many of these items we have 
+                count++;
+            }
+        }
+    }
+ // depending on what the keyword is, we should put our count in a certain row. we take the first character of our input argument (the letter of the column) to figure out which column we should add our count to
+    if (category === 'READ') {
+        read = count; 
+        learningTotal = read + watch; 
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '27').setValue(learningTotal);
+    }
+    if (category === 'WATCH') {
+        //watch is a 15 minute unit, so let's divide the 1 hour standard unit by 4 to get our accurate count for the watch category
+        watch = count/4; 
+        learningTotal = read + watch; 
+        // reading and watching are going to end up in the same category, but they use different keywords and different units of time. 
+        // add them together using the global scope variables so that we can get an accurate number for this combined category.
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '27').setValue(learningTotal);
+    }
+    if (category === 'DISCUSS') {
+        discuss = count; 
+        discussCollabTotal = discuss + collab; 
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '28').setValue(discussCollabTotal);
+    }
+    if (category === 'COLLABORATE') {
+        collab = count; 
+        // discussing and collaborating are going to end up in the same category, but they use different keywords. 
+        // add them together using the global scope variables so that we can get an accurate number for this combined category.
+        discussCollabTotal = discuss + collab; 
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '28').setValue(discussCollabTotal);
+    }
+    if (category === 'PRACTICE') {
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '29').setValue(count);
+        practice = count; 
+    }
+   if (category === 'REVIEW') {
         // reviewing, working, and submitting are going to end up in the same category, but they use different keywords. 
         // add them together using the global scope variables so that we can get an accurate number for this combined category.
         review = count; 
@@ -87,26 +95,36 @@ function getColumn(category){
         // add them together using the global scope variables so that we can get an accurate number for this combined category.
         work = count; 
         assignmentTotal = review + work + submit; 
-        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '29').setValue(assignmentTotal);
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '30').setValue(assignmentTotal);
     }
     if (category === 'SUBMIT') {
         // reviewing, working, and submitting are going to end up in the same category, but they use different keywords. 
         // add them together using the global scope variables so that we can get an accurate number for this combined category.
         submit = count/4; 
         assignmentTotal = review + work + submit; 
-        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '29').setValue(assignmentTotal);
+        SpreadsheetApp.getActiveSheet().getRange(currentColumn + '30').setValue(assignmentTotal);
     }
-  
-  }
-// when someone makes an edit to the spreadsheet, we should update our counts
+
+}
+
+// when someone makes an edit to the spreadsheet, we should check the active column for updates
 function onEdit(e) {
-    checkCurrentColumn();
+   checkCurrentColumn();
 }
 
 function checkCurrentColumn(){
-    for (var i = 0; i < categories.length; i++) {
-      getColumn(categories[i]);
-    }
+  for (var i = 0; i < categories.length; i++) {
+    getColumn(categories[i]);
   }
+}
 
-
+function checkAll(){
+  var alphabet = 'BCDEFGHIJK';
+  for (var j = 0; j < alphabet.length; j++) {
+    currentColumn = alphabet[j]; 
+   for (var i = 0; i < categories.length; i++) {
+   getValues(categories[i]);
+  }
+  }
+ 
+}
